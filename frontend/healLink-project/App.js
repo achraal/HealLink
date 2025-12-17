3c9ddaad5834ce78ced294e2bd633b2e60046f4d
anas325 <91235726+anas325@users.noreply.github.com>
2025-12-17 17:41:47 +0100

    Switched from Stack navigation to Tab navigation

--- frontend/healLink-project/App.js
+++ frontend/healLink-project/App.js
@@ -1,26 +1,45 @@
-import 'react-native-gesture-handler';
 
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen';
 import QRCodeScreen from './src/screens/qr_display';
 import QrScanner from './src/screens/qrscanner';
 import { WebSocketProvider } from './src/utils/websocketProvider';
+import { Ionicons } from '@expo/vector-icons'; 
 
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <WebSocketProvider>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Scan">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Suivi des Campagnes" }} />
-        <Stack.Screen name="QR" component={QRCodeScreen} options={{ title: "Afficher son QR" }} />
-        <Stack.Screen name="Scan" component={QrScanner} options={{ title: "Scanner un code QR" }} />
-      </Stack.Navigator>
+      <Tab.Navigator
+              screenOptions={({ route }) => ({
+                headerShown: false, // hide header bar
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+                  if (route.name === 'Home') {
+                    iconName = focused ? 'home' : 'home-outline';
+                  } else if (route.name === 'Qr scan') {
+                    iconName = focused ? 'information-circle' : 'information-circle-outline';
+                  } else if (route.name === 'Qr') {
+                    iconName = focused ? 'qr-code' : 'qr-code-outline';
+      
+                  }
+                  return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: '#007AFF',
+                tabBarInactiveTintColor: 'gray',
+              })}
+            >
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Qr scan" component={QrScanner} />
+              <Tab.Screen name="Qr" component={QRCodeScreen} />
+
+            </Tab.Navigator>
     </NavigationContainer>
     </WebSocketProvider>
   );
